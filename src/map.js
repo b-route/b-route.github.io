cc6bbbf02466988cb181e108aa277eeb2c079e07
b-route <70137192+b-route@users.noreply.github.com>
2020-08-24 15:39:21 +0300

    Update map.js

--- src/map.js
+++ src/map.js
@@ -4,25 +4,6 @@ ymaps.ready(init);
                 center: [59.57, 30.19],
                 zoom: 7
             });
-                
-            myGeoObject = new ymaps.GeoObject({
-            // Описание геометрии.
-            geometry: {
-                type: "Point",
-                coordinates: [55.8, 37.8]
-            },
-            // Свойства.
-            properties: {
-                // Контент метки.
-                iconContent: 'Я тащусь',
-                hintContent: 'Ну давай уже тащи'
-            }
-        }, {
-            // Опции.
-            // Иконка метки будет растягиваться под размер ее содержимого.
-            preset: 'islands#blackStretchyIcon',
-            // Метку можно перемещать.
-            draggable: true
-        }),
+            
         }
     
