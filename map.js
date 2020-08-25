b513af2ebf4c191734533912e9097cde1afe62e4
b-route <70137192+b-route@users.noreply.github.com>
2020-08-25 18:58:56 +0300

    Update map.js

--- map.js
+++ map.js
@@ -35,6 +35,26 @@ function init () {
          myMap.geoObjects
         .add(myGeoObject)
     
+    
+    
+     myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) {
+            var coords = e.get('coords');
+            myMap.balloon.open(coords, {
+                contentHeader:'Событие!',
+                contentBody:'<p>Кто-то щелкнул по карте.</p>' +
+                    '<p>Координаты щелчка: ' + [
+                    coords[0].toPrecision(6),
+                    coords[1].toPrecision(6)
+                    ].join(', ') + '</p>',
+                contentFooter:'<sup>Щелкните еще раз</sup>'
+            });
+        }
+        else {
+            myMap.balloon.close();
+        }
+    });
+    
     };
 
 
