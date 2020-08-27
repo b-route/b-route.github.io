dbb6b3ad6ed679f43e37e7287d330a2dde6a56a7
b-route <70137192+b-route@users.noreply.github.com>
2020-08-27 21:01:37 +0300

    Update map.js

--- map.js
+++ map.js
@@ -44,36 +44,18 @@ function init () {
             myMap.balloon.open(coords, {
                 contentHeader:'Событие!',
                 contentBody:'<p>Кто-то щелкнул по карте.</p>' +
-                    '<p>Координаты щелчка: </p>',
+                    '<p>Координаты щелчка:' coords.getAdressLine() ' </p>',
                 contentFooter:'<sup>Щелкните еще раз</sup>'
             });
         }
         else {
             myMap.balloon.close();
         }
-          getAddress(coords);
+          
     });
     
     
     
     };
 
- function getAddress(coords) {
-        myPlacemark.properties.set('iconCaption', 'поиск...');
-        ymaps.geocode(coords).then(function (res) {
-            var firstGeoObject = res.geoObjects.get(0);
-
-            myPlacemark.properties
-                .set({
-                    // Формируем строку с данными об объекте.
-                    iconCaption: [
-                        // Название населенного пункта или вышестоящее административно-территориальное образование.
-                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
-                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
-                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
-                    ].filter(Boolean).join(', '),
-                    // В качестве контента балуна задаем строку с адресом объекта.
-                    balloonContent: firstGeoObject.getAddressLine()
-                });
-        });
     }
