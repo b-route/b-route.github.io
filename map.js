9f98dfd24d74bc094fedc4cc64fd577fb6a2433c
b-route <70137192+b-route@users.noreply.github.com>
2020-08-28 18:51:25 +0300

    Update map.js

--- map.js
+++ map.js
@@ -111,7 +111,7 @@ function  TSP() {
    
         for (var i = 0; i < countOfAdresses+1; i++) {
             for (var j = i+1; j < countOfAdresses+1; j++) {
-                route = new ymaps.router( [
+                route = new ymaps.route( [
                 arrCoordinates[i],
                 arrCoordinates[j]
             ])
